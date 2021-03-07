a88f81d926591c1ed4fc8d8444d63b28f035f29a
aegooby <aegooby@gmail.com>
2021-03-08 01:45:41 +0300

    Removed unused Stripe code

--- components/App.tsx
+++ components/App.tsx
@@ -1,15 +1,9 @@
 
 import * as React from "react";
-import * as Stripe from "stripe";
 
 import Index from "./Pages/Index.tsx";
 import Order from "./Pages/Order.tsx";
 import * as UIRouter from "./Router/UIRouter.tsx";
-
-/** @todo Integrate. */
-const stripeAPIKey = "pk_test_51IPELvBCMz7QpSOWDOXR1BzczWDxi6ZqkJtiE6MN3grVj" +
-    "hk7L512MLB1ZSDwmRv1GNQbU2Mpnfo2SSCwNvxzr8mX00ZbZlstKm";
-const stripe = Stripe.loadStripe();
 
 export default class App extends React.Component
 {
