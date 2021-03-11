dff247b06282e0e9ea01b31b75c5aac0f0555e8a
aegooby <aegooby@gmail.com>
2021-03-12 01:10:22 +0300

    Reorganized client

--- client/bundle.tsx
+++ client/bundle.tsx
@@ -1,15 +1,15 @@
 
 import * as React from "https://esm.sh/react";
-import * as Client from "https://raw.githubusercontent.com/aegooby/httpsaurus/master/client/client.tsx";
+import * as client from "https://raw.githubusercontent.com/aegooby/httpsaurus/master/client/client.tsx";
 
 import App from "../components/Core/App.tsx";
 
 try
 {
-    const client = new Client.Client();
-    client.hydrate(<App />);
+    const httpclient = new client.Client();
+    httpclient.hydrate(<App />);
 }
 catch (error)
 {
-    Client.Console.log(error);
+    client.Console.log(error);
 }
