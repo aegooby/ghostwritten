6e1d0e285174927f8011a36b731b1da589b3e1de
aegooby <aegooby@gmail.com>
2021-03-25 09:29:17 +0300

    Adjusted meta description

--- components/Pages/Index.tsx
+++ components/Pages/Index.tsx
@@ -17,11 +17,14 @@ export default class Index extends React.Component<unknown, unknown>
             <>
                 <ReactHelmet.Helmet>
                     <title>Ghostwritten</title>
-                    <meta name="description" content={"You send us what you need. " +
-                        "Need an IA? Extended Essay? College application essay? " +
-                        "No problem. Tell us what you need and when, then leave " +
-                        "the rest to us. We guarantee an on-time, A-level essay " +
-                        "in your inbox."} />
+                    <meta name="description" content=
+                        {
+                            "Need an IA? Extended Essay? College application essay? " +
+                            "No problem. Tell us what you need and when, then leave " +
+                            "the rest to us. We guarantee an on-time, A-level essay " +
+                            "in your inbox."
+                        }
+                    />
                 </ReactHelmet.Helmet>
                 <div className="wrapper">
                     <div className="header">
