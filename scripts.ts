b3909b24bd596a585c8f28bec7105450dcdd8efe
aegooby <aegooby@gmail.com>
2021-04-03 20:50:04 +0300

    Fixing Docker volume

--- scripts.ts
+++ scripts.ts
@@ -255,7 +255,7 @@ yargs.default(Deno.args)
                 [
                     "docker", "run", "-it", "--init", "-p", "443:8443", "-p",
                     "80:8080", "-v", "/etc/letsencrypt/:/etc/letsencrypt/",
-                    "ghostwritten/server:latest", "deno", ...command.split(" "),
+                    "ghostwritten/server:latest", ...command.split(" "),
                     "remote", ...devFlag
                 ]
         };
