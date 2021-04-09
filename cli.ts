14de35af908409aa5d5a9e1480a76b24a98cdb65
aegooby <aegooby@gmail.com>
2021-04-09 22:20:38 +0300

    Moved Docker to background

--- cli.ts
+++ cli.ts
@@ -248,7 +248,7 @@ async function docker(args: Arguments)
     {
         cmd:
             [
-                "docker", "run", "-it", "--init", "-p", "443:8443", "-p",
+                "docker", "run", "-itd", "--init", "-p", "443:8443", "-p",
                 "80:8080", "-v", "/etc/letsencrypt/:/etc/letsencrypt/",
                 "ghostwritten/server:latest", ...command.split(" "),
                 "remote", ...devFlag
